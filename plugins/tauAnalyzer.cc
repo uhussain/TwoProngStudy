12e7f0bc79d32377399e8659e46fdef9c12fc53b
Laura Dodd <ldodd@cern.ch>
2015-05-04 10:22:14 -0500

    Made vector output- one entry per match for plotting script

--- plugins/tauAnalyzer.cc
+++ plugins/tauAnalyzer.cc
@@ -5,10 +5,10 @@
 // 
 /**\class tauAnalyzer tauAnalyzer.cc RecoTauTag/tauAnalyzer/plugins/tauAnalyzer.cc
 
- Description: [one line class summary]
+Description: [one line class summary]
 
- Implementation:
-     [Notes on implementation]
+Implementation:
+[Notes on implementation]
 */
 //
 // Original Author:  Isobel Ojalvo
@@ -54,36 +54,36 @@
 //
 
 class tauAnalyzer : public edm::EDAnalyzer {
-   public:
-      explicit tauAnalyzer(const edm::ParameterSet&);
-      ~tauAnalyzer();
+	public:
+		explicit tauAnalyzer(const edm::ParameterSet&);
+		~tauAnalyzer();
 
-      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+		static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 
 
-   private:
-      virtual void beginJob() override;
-      virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
-      virtual void endJob() override;
+	private:
+		virtual void beginJob() override;
+		virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+		virtual void endJob() override;
 
-      edm::InputTag tauSrc_;
-      edm::InputTag discriminatorSrc_;
-      TTree* tree;
-      std::vector<Float_t>* pts_;
-      std::vector<Int_t>* dmf_;
-      std::vector<Int_t>* passDiscr_;
-      std::vector<Int_t>* genMatchedJet_;
-      std::vector<Int_t>* genMatchedTau_;
-      std::vector<Float_t>* genMatchedPt_;
-      std::vector<Float_t>* jetPts_;
-      double maxDR_;
+		edm::InputTag tauSrc_;
+		edm::InputTag discriminatorSrc_;
+		TTree* tree;
+		std::vector<Float_t>* pts_;
+		std::vector<Int_t>* dmf_;
+		std::vector<Int_t>* passDiscr_;
+		std::vector<Int_t>* genMatchedJet_;
+		std::vector<Int_t>* genMatchedTau_;
+		std::vector<Float_t>* genMatchedPt_;
+		std::vector<Float_t>* jetPts_;
+		double maxDR_;
 
-      //virtual void beginRun(edm::Run const&, edm::EventSetup const&) override;
-      //virtual void endRun(edm::Run const&, edm::EventSetup const&) override;
-      //virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
-      //virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
+		//virtual void beginRun(edm::Run const&, edm::EventSetup const&) override;
+		//virtual void endRun(edm::Run const&, edm::EventSetup const&) override;
+		//virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
+		//virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
 
-      // ----------member data ---------------------------
+		// ----------member data ---------------------------
 };
 
 //
@@ -99,43 +99,43 @@ class tauAnalyzer : public edm::EDAnalyzer {
 //
 tauAnalyzer::tauAnalyzer(const edm::ParameterSet& cfg)
 {
-   //now do what ever initialization is needed
-  tauSrc_              = cfg.getParameter<edm::InputTag>("recoTau");
-  discriminatorSrc_    = cfg.getParameter<edm::InputTag>("recoTauDiscriminator");
+	//now do what ever initialization is needed
+	tauSrc_              = cfg.getParameter<edm::InputTag>("recoTau");
+	discriminatorSrc_    = cfg.getParameter<edm::InputTag>("recoTauDiscriminator");
 
-  edm::Service<TFileService> fs;
-  //ntuple additions
-  tree = fs->make<TTree>("Ntuple", "Ntuple");
-  pts_ = new std::vector<Float_t>();
-  dmf_ = new std::vector<Int_t>();
-  passDiscr_ = new std::vector<Int_t>();
-  genMatchedTau_ = new std::vector<Int_t>();
-  genMatchedJet_ = new std::vector<Int_t>();
-  genMatchedPt_ = new std::vector<Float_t>();
-  jetPts_ = new std::vector<Float_t>();
-  tree->Branch("pt", "std::vector<float>", &pts_);
-  tree->Branch("dmf", "std::vector<int>", &dmf_);
-  tree->Branch("passDiscr", "std::vector<int>", &passDiscr_);
-  tree->Branch("genMatchedJet", "std::vector<int>", &genMatchedJet_);
-  tree->Branch("genMatchedTau", "std::vector<int>", &genMatchedTau_);
-  tree->Branch("genMatchedPt", "std::vector<float>", &genMatchedPt_);
-  tree->Branch("jetPt", "std::vector<float>", &jetPts_);
-  maxDR_ = 0.5;
+	edm::Service<TFileService> fs;
+	//ntuple additions
+	tree = fs->make<TTree>("Ntuple", "Ntuple");
+	pts_ = new std::vector<Float_t>();
+	dmf_ = new std::vector<Int_t>();
+	passDiscr_ = new std::vector<Int_t>();
+	genMatchedTau_ = new std::vector<Int_t>();
+	genMatchedJet_ = new std::vector<Int_t>();
+	genMatchedPt_ = new std::vector<Float_t>();
+	jetPts_ = new std::vector<Float_t>();
+	tree->Branch("pt", "std::vector<float>", &pts_);
+	tree->Branch("dmf", "std::vector<int>", &dmf_);
+	tree->Branch("passDiscr", "std::vector<int>", &passDiscr_);
+	tree->Branch("genMatchedJet", "std::vector<int>", &genMatchedJet_);
+	tree->Branch("genMatchedTau", "std::vector<int>", &genMatchedTau_);
+	tree->Branch("genMatchedPt", "std::vector<float>", &genMatchedPt_);
+	tree->Branch("jetPt", "std::vector<float>", &jetPts_);
+	maxDR_ = 0.5;
 }
 
 
 tauAnalyzer::~tauAnalyzer()
 {
- 
-   // do anything here that needs to be done at desctruction time
-   // (e.g. close files, deallocate resources etc.)
-   delete pts_;
-   delete dmf_;
-   delete passDiscr_;
-   delete genMatchedJet_;
-   delete genMatchedTau_;
-   delete genMatchedPt_;
-   delete jetPts_;
+
+	// do anything here that needs to be done at desctruction time
+	// (e.g. close files, deallocate resources etc.)
+	delete pts_;
+	delete dmf_;
+	delete passDiscr_;
+	delete genMatchedJet_;
+	delete genMatchedTau_;
+	delete genMatchedPt_;
+	delete jetPts_;
 }
 
 
@@ -145,113 +145,138 @@ tauAnalyzer::~tauAnalyzer()
 // Get collection of generator particles with status 2
 
 std::vector<const reco::GenParticle*> getGenParticleCollection(const edm::Event& evt) {
-    std::vector<const reco::GenParticle*> output;
-    edm::Handle< std::vector<reco::GenParticle> > handle;
-    evt.getByLabel("genParticles", handle);
-    // Loop over objects in current collection
-    for (size_t j = 0; j < handle->size(); ++j) {
-      const reco::GenParticle& object = handle->at(j);
-      //if(fabs(object.pdgId())==15 && object.status() == 2) output.push_back(&object);
-      if(object.status() == 2) output.push_back(&object);
-    }
-  return output;
+	std::vector<const reco::GenParticle*> output;
+	edm::Handle< std::vector<reco::GenParticle> > handle;
+	evt.getByLabel("genParticles", handle);
+	// Loop over objects in current collection
+	for (size_t j = 0; j < handle->size(); ++j) {
+		const reco::GenParticle& object = handle->at(j);
+		//if(fabs(object.pdgId())==15 && object.status() == 2) output.push_back(&object);
+		if(object.status() == 2) output.push_back(&object);
+	}
+	return output;
 }
 
 
 // Get collection of pat::taus
 //
 std::vector<const reco::PFTau*> getRecoCandCollections(const edm::Event& evt, const edm::InputTag& collection) {
-    std::vector<const reco::PFTau*> output;
-    edm::Handle<std::vector<reco::PFTau> > handle;
-    evt.getByLabel(collection, handle);
-    // Loop over objects in current collection
-    for (size_t j = 0; j < handle->size(); ++j) {
-      const reco::PFTau& object = handle->at(j);
-      if(object.pt()>15. && fabs(object.eta())< 3.){
-        output.push_back(&object);
-      }
-    }
-  return output;
+	std::vector<const reco::PFTau*> output;
+	edm::Handle<std::vector<reco::PFTau> > handle;
+	evt.getByLabel(collection, handle);
+	// Loop over objects in current collection
+	for (size_t j = 0; j < handle->size(); ++j) {
+		const reco::PFTau& object = handle->at(j);
+		if(object.pt()>15. && fabs(object.eta())< 3.){
+			output.push_back(&object);
+		}
+	}
+	return output;
 }
 // Method to find the best match between tag tau and gen object. The best matched gen tau object will be returned. If there is no match within a DR < 0.5, a null pointer is returned
 const reco::GenParticle* findBestGenMatch(const reco::PFTau TagTauObj,
-    std::vector<const reco::GenParticle*>& GenPart, double maxDR) {
-  const reco::GenParticle* output = NULL;
-  double bestDeltaR = -1;
-  for (size_t i = 0; i < GenPart.size(); ++i) {
-    double deltaR = reco::deltaR(TagTauObj, *GenPart[i]);
-    if (deltaR < maxDR) {
-      if (!output || deltaR < bestDeltaR) {
-        output = GenPart[i];
-        bestDeltaR = deltaR;
-      }
-    }
-  }
-  return output;
+		std::vector<const reco::GenParticle*>& GenPart, double maxDR) {
+	const reco::GenParticle* output = NULL;
+	double bestDeltaR = -1;
+	for (size_t i = 0; i < GenPart.size(); ++i) {
+		double deltaR = reco::deltaR(TagTauObj, *GenPart[i]);
+		if (deltaR < maxDR) {
+			if (!output || deltaR < bestDeltaR) {
+				output = GenPart[i];
+				bestDeltaR = deltaR;
+			}
+		}
+	}
+	return output;
 }
 
 // ------------ method called for each event  ------------
-void
+	void
 tauAnalyzer::analyze(const edm::Event& evt, const edm::EventSetup& iSetup)
 {
-  using namespace edm;
-  Handle<reco::PFTauCollection> tauObjects;
-  evt.getByLabel(tauSrc_, tauObjects);
-  
-  edm::Handle<reco::PFTauDiscriminator> discriminator;
-  evt.getByLabel(discriminatorSrc_, discriminator);
+	using namespace edm;
+	Handle<reco::PFTauCollection> tauObjects;
+	evt.getByLabel(tauSrc_, tauObjects);
 
-  edm::Handle<reco::PFTauDiscriminator> DMF; 
-  evt.getByLabel("hpsPFTauDiscriminationByDecayModeFinding",DMF);
+	edm::Handle<reco::PFTauDiscriminator> discriminator;
+	evt.getByLabel(discriminatorSrc_, discriminator);
 
-  pts_->clear();
-  dmf_->clear();
-  passDiscr_->clear();
-  genMatchedTau_->clear();
-  genMatchedJet_->clear();
-  genMatchedPt_->clear();
-  jetPts_->clear();
-  for (unsigned int iTau = 0; iTau<tauObjects->size() ; ++iTau){
-        reco::PFTauRef tauCandidate(tauObjects, iTau);
-        pts_->push_back(tauCandidate->pt());
-        // check if tau candidate has passed discriminator
-        if( (*DMF)[tauCandidate] > 0.5 ) dmf_->push_back(1);
-        else dmf_->push_back(0);
-        // check if tau candidate has passed discriminator
-        if( (*discriminator)[tauCandidate] > 0.5 ){
-        // do something with your candidate
-        passDiscr_->push_back(1);
-        }
-        else{
-        passDiscr_->push_back(0);
-        }
-  }//end tau loop 
+	edm::Handle<reco::PFTauDiscriminator> DMF; 
+	evt.getByLabel("hpsPFTauDiscriminationByDecayModeFinding",DMF);
 
-  
-  std::vector<const reco::GenParticle*> GenObjects = getGenParticleCollection(evt);
-  for(unsigned int iTau = 0; iTau<tauObjects->size(); iTau++){
-	  const reco::PFTau tauCand=tauObjects->at(iTau);
-	  const reco::Candidate* bestGenMatch = findBestGenMatch(tauCand,GenObjects, maxDR_) ;
-	  if(bestGenMatch) {
-		  genMatchedPt_->push_back(bestGenMatch->pt());
-		  if (abs(bestGenMatch->pdgId())==15){
-			  genMatchedTau_->push_back(1);
-			  genMatchedJet_->push_back(0);
-		  }
-		  else{
-			  genMatchedJet_->push_back(1);//Not Necessarily a jet, but certainly not a tau
-			  genMatchedTau_->push_back(0);}
-	  }
-	  else {
-		  genMatchedPt_->push_back(-1);
-		  genMatchedJet_->push_back(-1);
-		  genMatchedTau_->push_back(-1);
-	  }
-  //if (PFJetRef& jetRef_()
-  jetPts_->push_back(0);
-  }
+	pts_->clear();
+	std::vector<float> pts;
+	dmf_->clear();
+	std::vector<int> dmf;
+	passDiscr_->clear();
+	std::vector<int> passDiscr;
+	genMatchedTau_->clear();
+	std::vector<int> genMatchedTau;
+	genMatchedJet_->clear();
+	std::vector<int> genMatchedJet;
+	genMatchedPt_->clear();
+	std::vector<float> genMatchedPt;
+	jetPts_->clear();
+	std::vector<float> jetPts;
 
-  tree->Fill();
+	for (unsigned int iTau = 0; iTau<tauObjects->size() ; ++iTau){
+		reco::PFTauRef tauCandidate(tauObjects, iTau);
+		pts.push_back(tauCandidate->pt());
+		// check if tau candidate has passed discriminator
+		if( (*DMF)[tauCandidate] > 0.5 ) dmf.push_back(1);
+		else dmf.push_back(0);
+		// check if tau candidate has passed discriminator
+		if( (*discriminator)[tauCandidate] > 0.5 ){
+			// do something with your candidate
+			passDiscr.push_back(1);
+		}
+		else{
+			passDiscr.push_back(0);
+		}
+	}//end tau loop 
+
+
+	std::vector<const reco::GenParticle*> GenObjects = getGenParticleCollection(evt);
+	for(unsigned int iTau = 0; iTau<tauObjects->size(); iTau++){
+		const reco::PFTau tauCand=tauObjects->at(iTau);
+		const reco::Candidate* bestGenMatch = findBestGenMatch(tauCand,GenObjects, maxDR_) ;
+		if(bestGenMatch) {
+			genMatchedPt.push_back(bestGenMatch->pt());
+			if (abs(bestGenMatch->pdgId())==15){
+				genMatchedTau.push_back(1);
+				genMatchedJet.push_back(0);
+			}
+			else{
+				genMatchedJet.push_back(1);//Not Necessarily a jet, but certainly not a tau
+				genMatchedTau.push_back(0);
+			}
+		}
+		else {
+			genMatchedPt.push_back(-1);
+			genMatchedJet.push_back(-1);
+			genMatchedTau.push_back(-1);
+		}
+		//if (PFJetRef& jetRef_()
+		jetPts.push_back(0);
+	}
+	for (size_t i = 0; i < tauObjects->size(); ++i) {
+		pts_->clear();
+		dmf_->clear();
+		passDiscr_->clear();
+		genMatchedTau_->clear();
+		genMatchedJet_->clear();
+		genMatchedPt_->clear();
+		jetPts_->clear();
+
+		pts_->push_back(pts.at(i));
+		dmf_->push_back(dmf.at(i));
+		passDiscr_->push_back(passDiscr.at(i));
+		genMatchedTau_->push_back(genMatchedTau.at(i));
+		genMatchedJet_->push_back(genMatchedJet.at(i));
+		genMatchedPt_->push_back(genMatchedPt.at(i));
+		jetPts_->push_back(jetPts.at(i));
+		tree->Fill();  // create TTree
+	}
 
 }
 
