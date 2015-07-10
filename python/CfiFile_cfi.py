1967dbda028a8252f41c7671811e5514947efaee
Laura Dodd <ldodd@cern.ch>
2015-07-10 08:28:09 -0500

    removing python building files

--- python/CfiFile_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-tauAnalyzer = cms.EDAnalyzer('tauAnalyzer',
-                      recoTau              = cms.InputTag("hpsPFTauProducer"),
-                      recoTauDiscriminator = cms.InputTag("hpsPFTauDiscriminationByLooseIsolation")
-
-)
