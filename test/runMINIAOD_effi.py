ee44fc808b02819695f3989469a2d11ec54f7ad0
Aaron Markowitz <markowitz@college.harvard.edu>
2015-07-10 08:51:27 -0500

    July 10 final

--- test/runMINIAOD_effi.py
+++ test/runMINIAOD_effi.py
@@ -1,16 +1,22 @@
 import FWCore.ParameterSet.Config as cms
 from FWCore.ParameterSet.VarParsing import VarParsing
+
+#input cmsRun options
 options = VarParsing ('analysis')
 options.inputFiles = '/store/mc/RunIISpring15DR74/SUSYGluGluToHToTauTau_M-250_TuneCUETP8M1_13TeV-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/80000/6EBE4F55-4E03-E511-9FE6-0CC47A13D416.root'
 options.outputFile = "MiniAOD_effi.root"
 options.parseArguments()
-process = cms.Process("TreeProducerFromMiniAOD")
 
+#name the process
+process = cms.Process("TreeProducerFromMiniAOD")
 process.load('FWCore/MessageService/MessageLogger_cfi')
 process.MessageLogger.cerr.FwkReport.reportEvery = 100;
 process.MessageLogger.cerr.threshold = cms.untracked.string('INFO')
-#process.GlobalTag.globaltag = cms.string('PHYS14_25_V2::All')
 
+#50 ns global tag for MC replace with 'GR_P_V56' for prompt reco. https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideFrontierConditions#Prompt_reconstruction_Global_Tag 
+process.GlobalTag.globaltag = 'MCRUN2_74_V9A'
+
+#how many events to run over
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
 )
@@ -118,5 +124,7 @@ process.TFileService = cms.Service("TFileService",
         fileName = cms.string(options.outputFile)
 )
 
-dump_file = open('dump.py','w')
-dump_file.write(process.dumpPython())
+#print out all processes used when running- useful check to see if module ran
+#UNCOMMENT BELOW
+#dump_file = open('dump.py','w')
+#dump_file.write(process.dumpPython())
