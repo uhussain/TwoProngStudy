0d901c13de1013b21b3faec1c0767b5a89941cdf
Nick Cinko <ncinko@purdue.edu>
2016-07-20 10:57:41 -0500

    cleanup

--- test/farmout.sh
+++ test/farmout.sh
@@ -5,82 +5,12 @@ if [ $# -ne $EXPECTED_ARGS ]
 then
   echo "Usage: $0 JOB_NAME"
 fi
-farmoutAnalysisJobs $1-WJets_signal \
-  --input-files-per-job=1 \
-  --job-generates-output-name \
-  --infer-cmssw-path \
-  --input-file-list=HTauTau.txt \
-  --input-dir=root://cmsxrootd.fnal.gov/ \
-  --assume-input-files-exist \
-  ./runMINIAOD_FR.py  \
-  'inputFiles=$inputFileNames' 'outputFile=$outputFileName'
 
-farmoutAnalysisJobs $1-WJets_orig \
-  --input-files-per-job=1 \
+farmoutAnalysisJobs $1 \
+  --input-files-per-job=3 \
   --job-generates-output-name \
   --infer-cmssw-path \
-  --input-file-list=WJets.txt \
-  --input-dir=root://cmsxrootd.fnal.gov/ \
+  --input-file-list=/afs/hep.wisc.edu/home/ncinko/private/CMSSW_8_0_10/src/RecoTauTag/tauAnalysis/test/files_2prong.txt\
   --assume-input-files-exist \
-  ./runMINIAOD_FR.py  \
-  'inputFiles=$inputFileNames' 'outputFile=$outputFileName'
-
-farmoutAnalysisJobs $1-ggHtautau_250 \
-  --input-files-per-job=1 \
-  --job-generates-output-name \
-  --infer-cmssw-path \
-  --input-file-list=HTauTau.txt \
-  --input-dir=root://cmsxrootd.fnal.gov/ \
-  --assume-input-files-exist \
-  ./runMINIAOD_effi.py  \
-  'inputFiles=$inputFileNames' 'outputFile=$outputFileName'
-
-farmoutAnalysisJobs $1-ggHtautau_800 \
-  --input-files-per-job=1 \
-  --job-generates-output-name \
-  --infer-cmssw-path \
-  --input-file-list=HTauTau_800.txt \
-  --input-dir=root://cmsxrootd.fnal.gov/ \
-  --assume-input-files-exist \
-  ./runMINIAOD_effi.py  \
-  'inputFiles=$inputFileNames' 'outputFile=$outputFileName'
-
-farmoutAnalysisJobs $1-ggHtautau_1400 \
-  --input-files-per-job=1 \
-  --job-generates-output-name \
-  --infer-cmssw-path \
-  --input-file-list=HTauTau_1400.txt \
-  --input-dir=root://cmsxrootd.fnal.gov/ \
-  --assume-input-files-exist \
-  ./runMINIAOD_effi.py  \
-  'inputFiles=$inputFileNames' 'outputFile=$outputFileName'
-
-farmoutAnalysisJobs $1-ggHtautau_2600 \
-  --input-files-per-job=1 \
-  --job-generates-output-name \
-  --infer-cmssw-path \
-  --input-file-list=HTauTau_2600.txt \
-  --input-dir=root://cmsxrootd.fnal.gov/ \
-  --assume-input-files-exist \
-  ./runMINIAOD_effi.py  \
-  'inputFiles=$inputFileNames' 'outputFile=$outputFileName'
-
-farmoutAnalysisJobs $1-QCD_15TTo \
-  --input-files-per-job=1 \
-  --job-generates-output-name \
-  --infer-cmssw-path \
-  --input-file-list=QCD_15TTo7000.txt \
-  --input-dir=root://cmsxrootd.fnal.gov/ \
-  --assume-input-files-exist \
-  ./runMINIAOD_FR_QCD.py  \
-  'inputFiles=$inputFileNames' 'outputFile=$outputFileName'
-
-farmoutAnalysisJobs $1-QCD_15To \
-  --input-files-per-job=1 \
-  --job-generates-output-name \
-  --infer-cmssw-path \
-  --input-file-list=QCD.txt \
-  --input-dir=root://cmsxrootd.fnal.gov/ \
-  --assume-input-files-exist \
-  ./runMINIAOD_FR_QCD.py  \
+  ./runMINIAODFR_2prong.py  \
   'inputFiles=$inputFileNames' 'outputFile=$outputFileName'
